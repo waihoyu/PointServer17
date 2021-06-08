98637f105af832cff91240bcf6c98d7f64e991cf
waihoyu <13701343809@qq.com>
2021-06-08 09:20:27 +0800

    '注册'

--- routes/register.js
+++ routes/register.js
@@ -4,6 +4,32 @@ var router = express.Router();
 
 /* GET register listing. */
 //req request 请求 问
+
+
+router.post('/check', function(req, res, next) {
+  let inform = req.body.params
+  let username = inform.newusername
+  let password = inform.newpassword
+  let existUserSQL = 'select user_name from users';
+  let conn = db.connection()
+  db.query1(conn,existUserSQL,{username:username},function(resx){
+    if(resx){
+      res.send({
+        state:"failed",
+        message:"用户名不可以用"
+      })
+    }else{
+      res.send({
+        state:"success",
+        message:"用户名可以用"
+      })
+    }
+  });
+  db.close(conn)
+
+})
+
+
 //res response
 router.get('/', function(req, res, next) {
   
@@ -17,7 +43,19 @@ router.get('/', function(req, res, next) {
   res.send(result)
 });
 
-router.post('/check',function(req,res,next){
+router.post('/temp2', function(req, res, next) {
+  let inform = req.body.params;
+  let username = inform.newusername;
+  let password = inform.newpassword;
+  // let existUserSQL = 'select user_name from users';
+  let conn = db.connection()
+  // db.query1(conn,existUserSQL,{username:username},function(resx){
+  //   if(resx){
+  //     res.send("重名了")
+  //   }else{
+  //     res.send("用户名可以用")
+})
+router.post('/check2',function(req,res,next){
   let inform = req.body.params;
   let username = inform.newusername;
   // let password = inform.newpassword;
@@ -38,13 +76,10 @@ router.post('/check',function(req,res,next){
   });
   db.close(conn)
 
-})
+});
 
 
 router.post('/', function(req, res, next) {
-  
-
-
   // let newUserSQL = 'INSERT INTO USERS (user_id,user_name,user_password,user_create_time) VALUES(1002,' +'"'+ username+'"' + ',' +'"'+ password +'"' +',' +'"' + new Date().format('yyyy-MM-dd hh:mm:ss') + '"' + ')' 
   // let conn = db.connection()
   // // let result = {}
@@ -55,15 +90,32 @@ router.post('/', function(req, res, next) {
   //   }
   //   else{
   //     result.state= "failed"
-  //   }
-  //   // console.log(result)
-  //   res.send(result);
-  // })
-  // db.close(conn)
 
-  
+  //   }
+  // });
+  // db.close(conn)
+  let inform = req.body.params;
+  let username = inform.newusername;
+  let password = inform.newpassword;
+  let newUserSQL = 'INSERT INTO USERS (user_id,user_name,user_password,user_create_time) VALUES(1002,' +'"'+ username+'"' + ',' +'"'+ password +'"' +',' +'"' + new Date().format('yyyy-MM-dd hh:mm:ss') + '"' + ')' 
+
+  let conn = db.connection()
+  // let result = {}
+  console.log(newUserSQL)
+  db.insert(conn,newUserSQL,'',function(resx){
+    let result = resx;
+    if(resx.insertId ==0 ){
+
+      result.state="success"
+    }
+    else{
+      result.state= "failed"
+    }
+    // console.log(result)
+    res.send(result);
+  })
+  db.close(conn)
 
 });
-
 
 module.exports = router;
